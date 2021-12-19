1c112cd9cc3ec81425413c43b4c0049580c3b34e
matinyan_0 <matinyan_0@mail.ru>
2021-12-19 21:18:10 +0400

    Simple SSR

---
+++ src/server.tsx
@@ -0,0 +1,21 @@
+// @ts-ignore
+import React from "react";
+import express, { Request, Response } from "express";
+import { renderToString } from "react-dom/server";
+import path from "path";
+import App from "./App";
+import template from "./template";
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "./dist/public")));
+
+// @ts-ignore
+app.get("/", (req: Request, res: Response) => {
+    const body = renderToString(<App />);
+    const html = template(body);
+
+    res.send(html);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
